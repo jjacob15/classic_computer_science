5733781dc17744ef11e214d820e56e8b79953df6
jjacob <jaison.jack@gmail.com>
2024-07-15 10:44:54 +0800

    fraud median two ways

--- array/sorting/fraud_activity.py
+++ array/sorting/fraud_activity.py
@@ -1,27 +1,93 @@
+import heapq
 def fraud(expenditure,d):
-    isEven = d % 2 == 0
-    window = sorted(expenditure[:d])
-    notices = 0
-
-    def insert_sorted(arr, value):
-        for i in range(len(arr)):
-            if value < arr[i]:
-                arr.insert(i, value)
-                return
-        arr.append(value)
-
-    for i in range(d, len(expenditure)):
-        medianTransaction = 0
-        if isEven:
-            medianTransaction = (window[d//2 - 1] + window[d//2]) / 2
+    def add(num,low,high):
+        if not low or num <= -low[0]:
+            heapq.heappush(low, -num)
         else:
-            medianTransaction = window[d//2]
+            heapq.heappush(high, num)
+        balance(low, high)
+
+    def remove(num,low,high):
+        if num <=-low[0]:
+            low.remove(-num)
+            heapq.heapify(low)
+        else:
+            high.remove(num)
+            heapq.heapify(high)
+        balance(low,high)
         
-        if expenditure[i] >= medianTransaction * 2:
-            notices += 1
+    def balance(low,high):
+        if len(low) > len(high) + 1:
+            heapq.heappush(high, -heapq.heappop(low))
+        elif len(high) > len(low):
+            heapq.heappush(low, -heapq.heappop(high))
+
+
+    def get_median(low,high,d):
+        if d%2==1:
+            return -low[0]
+        return (-low[0] + high[0]) / 2
+
+    notification =0
+    low,high = [],[]
+
+    print(d)
+    for num in expenditure[:d]:
+        add(num, low, high)
+
+    for i in range(d,len(expenditure)):
+        median = get_median(low,high,d)
+        print(median, low, high)    
+        if expenditure[i] >=2 *median:
+            notification+=1
         
-        # Update the window
-        window.remove(expenditure[i - d])
-        insert_sorted(window, expenditure[i])
+        remove(expenditure[i-d],low,high)
+        add(expenditure[i], low, high)
+
+        print(low,high)            
+
+    return notification
+
+
+
+
+
+
+
+
+
+
+
+
+def fraud_with_array(expenditure,d):
+    def get_median(counts,d):
+        count =0
+        median_pos = (d//2) + 1
+        if d%2==0:
+            first = None
+            for i,c in enumerate(counts):
+                count += c
+                if count >= median_pos and first is None:
+                    first = i
+                if count >= median_pos:
+                    return (first + i)/2
+        else:
+            for i, c in enumerate(counts):
+                count +=c
+                if count >=  median_pos:
+                    return i
     
-    return notices
+    count = [0] * 201
+    notification = 0
+    for i in range(d):
+        count[expenditure[i]] +=1
+    
+    for i in range(d,len(expenditure)):
+        median = get_median(count,d)
+        if expenditure[i] >= median * 2:
+            notification +=1
+
+        count[expenditure[i]] +=1
+        count[expenditure[i-d]] -=1
+
+    return notification
