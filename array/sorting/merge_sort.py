86477928873c954d8c72756deb53303fa794d196
jjacob <jaison.jack@gmail.com>
2024-07-17 10:39:41 +0800

    sorting

--- array/sorting/merge_sort.py
+++ array/sorting/merge_sort.py
@@ -1,3 +1,46 @@
+
+def merge_sort_count(arr):
+    global merge_count
+    merge_count =0
+    merge_sort_count_helper(arr)
+    return merge_count
+
+
+def merge_sort_count_helper(arr):
+    global merge_count
+    if len(arr) > 1:
+        mid = len(arr)//2
+        left = arr[:mid]
+        right = arr[mid:]
+
+        merge_sort_count_helper(left)
+        merge_sort_count_helper(right)
+
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                arr[k] = left[i]
+                i+=1
+                k+=1
+            else:
+                arr[k] = right[j]
+                j+=1
+                merge_count += len(left) - i #to find the merge count, we are looking for len(left) - i postion
+                k+=1
+
+
+        while i < len(left):
+            arr[k] = left[i]
+            i+=1
+            k+=1
+
+        while j < len(right):
+            arr[k] = right[j]
+            j+=1
+            k+=1
+
+
+
 # complexity is O(nlogn) in the best/average and worst case. Space complexity is O(n) as it requires temp space during merge
 def merge_sort(arr):
    if len(arr) > 1:
--- array/sorting/test_sorting.py
+++ array/sorting/test_sorting.py
@@ -2,12 +2,19 @@ from bubble_sort import bubble_sort
 from selection_sort import selection_sort
 from insertion_sort import insertion_sort
 from merge_sort import merge_sort
-from merge_sort import bottom_up_merge_sort
+from merge_sort import bottom_up_merge_sort,merge_sort_count
 from quick_sort import quick_sort
 from bribe import bribe
 from fraud_activity import fraud
 
 sorted = [1, 2, 4, 4, 4, 5, 7, 12, 22, 23, 32, 332]
+
+def test_merge_sort_count():
+    assert merge_sort_count([1, 1, 1, 2, 2]) ==0
+    assert merge_sort_count([2, 1, 3, 1, 2]) ==4
+    assert merge_sort_count([1, 5, 3, 7]) ==1
+    assert merge_sort_count([7, 5, 3, 1]) == 6
+
 
 def test_fraud_activity():
     assert fraud([2, 3, 4, 2, 3, 6, 8, 4, 5],5) == 2
@@ -42,8 +49,11 @@ def test_insertion_sort():
 def test_merge_sort():
     arr =[32,22,1,5,2,4,12,4,7,23,4,332]
     merge_sort(arr)
-
     assert arr == sorted
+    
+    arr2 = [7, 5, 3, 1]
+    merge_sort(arr2)
+    assert arr2 == [1,3,5,7]
 
     
 def test_bottom_upmerge_sort():
