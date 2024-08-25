ee275fbcaa7d690e5fe17d1b3c0579641bd7a986
jjacob <jaison.jack@gmail.com>
2024-08-25 17:46:51 +0800

    updates

--- array/10_stack_queue/largest_rectange.py
+++ array/10_stack_queue/largest_rectange.py
@@ -1,2 +1,21 @@
+# INSIGHT -> area of a histogram. A histogram is bar charts usually representing some distribution. To 
+# find the are of a historgram, we add the index and height into a stack for all ascending bars until 
+# we find a building/bar thats smaller. Then you extract all bar thats taller that the current one and update
+# the its to our current index.
+# INSIGHT -> You do this to ensure you get the start for the width from all the buildings taller that this.
+ 
 def largest_rectange(h):
-    return 10
+    stack = []
+    max_area = 0
+    for i, bh in enumerate(h):
+        while stack and stack[-1][1] > bh:
+            index,height = stack.pop()
+            max_area = max(max_area, (i-index)*height)
+            i = index
+        stack.append((i,bh))
+
+    while stack:
+        index,height = stack.pop()
+        max_area = max(max_area,(len(h)-index)*height)
+
+    return max_area
--- array/10_stack_queue/test_stack_queue.py
+++ array/10_stack_queue/test_stack_queue.py
@@ -1,4 +1,11 @@
 from largest_rectange import largest_rectange
+from min_max_riddle import min_max_riddle
 
+def test_min_max_riddle():
+    assert min_max_riddle([6,3,5,1,12]) == [12,3,3,1,1]
+
+    
 def test_largest_rectangle():
     assert largest_rectange([3,2,3]) == 6
+    assert largest_rectange([1,3,5,9,11]) == 18
+    assert largest_rectange([11,11,10,10,10]) == 50
