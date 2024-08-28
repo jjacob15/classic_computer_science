d415828dc0b4d5e19a3f49031084a208a31782f3
jjacob <jaison.jack@gmail.com>
2024-08-28 16:23:57 +0800

    last local update

--- array/10_stack_queue/largest_rectange.py
+++ array/10_stack_queue/largest_rectange.py
@@ -1,2 +1,17 @@
-def largest_rectange(h):
-    return 10
+# INSIGHT ->
+def largest_rectange(heights):
+    stack = []
+    max_area = 0
+
+    for i,h in enumerate(heights):
+        start = i
+        while stack and stack[-1][1] > h:
+            index,height = stack.pop()
+            max_area = max(max_area, height * (i- index))
+            start = index
+        stack.append((start,h))
+
+    for i,h in stack:
+        max_area = max(max_area, h * (len(heights)- i))
+
+    return max_area
--- array/10_stack_queue/test_stack_queue.py
+++ array/10_stack_queue/test_stack_queue.py
@@ -2,3 +2,4 @@ from largest_rectange import largest_rectange
 
 def test_largest_rectangle():
     assert largest_rectange([3,2,3]) == 6
+    # assert largest_rectange([2, 1, 5, 6, 2, 3]) == 10
