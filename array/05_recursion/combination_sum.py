9efaa1a4c5826ac1d5b377a91ed78cbf6b293f8d
jjacob <jaison.jack@gmail.com>
2024-10-31 21:50:49 +0800

    backtracking dfs

--- array/05_recursion/combination_sum.py
+++ array/05_recursion/combination_sum.py
@@ -1,22 +1,46 @@
+                                
+#                                 candidates = [2, 3] and target = 4
+
+#                                              findCombinations(0, 4, [])
+#                                    /                                      \
+#                         Include 2 /                                        \ Exclude 2
+#                                  /                                          \
+#               findCombinations(0, 2, [2])                                    findCombinations(1, 4, [])
+#                      /              \                                               /          \
+#         Include 2 /                  \ Exclude 2                          Include 3 /           \ Exclude 3
+#                  /                    \                                             /             \
+# findCombinations(0, 0, [2, 2])   findCombinations(1, 2, [2])   findCombinations(1, 1, [3])   findCombinations(2, 4, [])
+
+#                 |                     |                       |                         |
+#                [2, 2]             (invalid)               (invalid)                 (invalid)
+
+
+#INSIGHT-> this is DFS using  backtracking. when you hit the leaf, you backtrack, exlude the last item and then DO NOT CHANGE THE REMAINING. This is
+# to now try with the right node. 
+
 def combintationSum(arr,target):
     result = []
-
-    def backtrack(idx,remain,comb):
-        if idx == len(arr):
-            return
-        if remain == target:
-            result.append(list(comb))
+    
+    def backtrack(remaining,start,combination):
+        if remaining == 0:
+            result.append(list(combination))
+            return 
         
-        for i in range(idx,len(arr)):
-            comb.append(arr[i])
-            backtrack(i+1,target -arr[i],comb)
-            comb.pop()
+        if start ==len(arr) or remaining < 0:
+            return
+        
+        combination.append(arr[start])
+        backtrack(remaining - arr[start],start,combination) 
+        combination.remove(arr[start])
+        print(remaining,combination)
+        backtrack(remaining,start+1,combination)
+
+    backtrack(target,0,[])
 
 
-    backtrack(0,target,[])
+    print(result)
+        
 
-    return result
-    
-    
+
 ans= combintationSum([2,3,5],8)
 print(ans)
